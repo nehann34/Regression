0a8ff8e089c48ad065fd97c358406de9abf1e440
Neha Nagpal <nehanagpal.nn34@gmail.com>
2018-05-11 13:54:18 +0530

    all regressions donegit add server.py

--- server.py
+++ server.py
@@ -15,8 +15,9 @@ def rec_whole_size(sock, size):
 
 
 def hypothesis_logistic(X,theta):
-	z=X.dot(theta)
+	z=np.dot(X,theta)
 	return (1 / (1 + np.exp(-z)))
+
 
 
 
@@ -44,14 +45,7 @@ def cost_linear(Y,y,m):
 	return J
 
 
-def cost_logistic(y,Y,m):
-	t=-1/m
-	l=np.log10(Y)
-	b=np.ones((m,1),dtype=int)
-	p=np.subtract(b,y)
-	q=np.subtract(b,l)
-	J=t*((np.multiply(y,l) + np.multiply(p,q)).sum())
-	return J
+
 
 def cost_regularized_linear(y,Y,m,theta,lambd):
 	t_m=2*m
@@ -66,41 +60,56 @@ def gradient_descent_linear(X,y,alpha,theta,m,n):
 		Y=hypothesis_linear(X,theta)
 		gr=np.matrix(p*(np.dot(np.transpose(Y-y) , X)))
 		theta=theta-np.transpose(gr)
-		print(theta)
-	print(theta)		
+		#print(theta)
+	return theta		
 	
 def gradient_descent_linear_regularization(X,y,alpha,lambd,theta,m,n):
-	for k in range(1,201):
-		p=float(alpha)/m
-		q=float(lambd) *p	
+	p=float(alpha)/m
+	for k in range(1,3):	
 		Y=hypothesis_linear(X,theta)	
-		grad=np.matrix([p*(np.subtract(Y,y).sum())])
-		for i in range(1,n):
-			for j in range(0,m):
-				P=p*(np.multiply(np.subtract(Y,y),X[:,i]).sum())
-			grad=np.vstack((grad,[P]))
-		theta=np.subtract(theta*(1-q),grad)
-	print(theta)	
+		gr1=np.matrix(p*(np.dot(np.transpose(Y-y) , X)))
+		gr2=float(lambd)*theta
+		gr=np.add(np.transpose(gr1), gr2)
+		theta=theta-p*gr
+	return theta	
 
+def cost_logistic(y,Y,m):
+	t=-1/m
+	l=np.log(Y)
+	b=np.ones((m,1),dtype=int)
+	p=np.subtract(b,y)
+	q=np.subtract(b,l)
+	J=t*((np.multiply(y,l) + np.multiply(p,q)).sum())
+	return J
 
-
-
+def cost_regularized_logistic(y,Y,m,theta,lambd):
+	t=-1/m
+	l=np.log(Y)
+	b=np.ones((m,1),dtype=int)
+	p=np.subtract(b,y)
+	q=np.subtract(b,l)
+	e=t*((np.multiply(y,l) + np.multiply(p,q)).sum())
+	f=(float(lambd)/(2*m))*(np.square(theta).sum())
+	J=np.add(e,f)
+	return J
 
 
 
 def gradient_descent_logistic(X,y,alpha,theta,m,n):
-	for k in range(1,201):
+	for k in range(1,3):
 		Y=hypothesis_logistic(X,theta)
-		grad=np.matrix([float(alpha)*(np.subtract(Y,y).sum())])
-		for i in range(1,n):
-			for j in range(0,m):
-				P=float(alpha)*(np.multiply(np.subtract(Y,y),X[:,i]).sum())
-			grad=np.vstack((grad,[P]))
-		theta=np.subtract(theta,grad)
+		gr=np.matrix(float(alpha)*(np.dot(np.transpose(Y-y) , X)))
+		theta=theta-np.transpose(gr)
+		
 	return theta
 
-
-
+def gradient_descent_logistic_regularization(X,y,alpha,lambd,theta,m,n):
+	#for k in range(1,3):
+		Y=hypothesis_logistic(X,theta)
+		gr1=np.matrix((1/m)*(np.dot(np.transpose(Y-y) , X)))
+		gr2=(float(lambd)/m) * theta
+		theta=theta-float(alpha)*(np.add(np.transpose(gr1) ,gr2))
+		return theta
 
 
 #recieve input sent by client
@@ -133,10 +142,10 @@ def rec_from_client(client_sock):
 		print("file recieved")
 
 		#if n_value <= 5 & type_r is 'linear':
-		X,y,theta=create_matrix_from_dataset(uname,m_value,n_value)
-		Y=hypothesis_linear(X,theta)
-		J=cost_linear(Y,y,m_value)
-		theta=gradient_descent_linear(X,y,alpha,theta,m_value,n_value)
+		#X,y,theta=create_matrix_from_dataset(uname,m_value,n_value)
+		#Y=hypothesis_linear(X,theta)
+		#J=cost_linear(Y,y,m_value)
+		#theta=gradient_descent_linear(X,y,alpha,theta,m_value,n_value)
 
 
 		#if n_value <= 5 & type_r is 'logistic':
@@ -155,14 +164,10 @@ def rec_from_client(client_sock):
 
 
 		#if n_value > 5 & l[1]='logistic':	
-
-
-
-
-
-
-
-
+		X,y,theta=create_matrix_from_dataset(uname,m_value,n_value)
+		Y=hypothesis_logistic(X,theta)
+		J=cost_regularized_logistic(y,Y,m_value,theta,lambd)
+		theta=gradient_descent_logistic_regularization(X,y,alpha,lambd,theta,m_value,n_value)
 
 
 
